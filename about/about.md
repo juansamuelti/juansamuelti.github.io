9fb5500bb659b99633f45a2ebc3646ec5111b062
Juan Samuel FP <juansamuelfp@outlook.com>
2024-05-26 17:38:43 -0400

    Updates

--- about/about.md
+++
@@ -1,8 +0,0 @@
----
-layout: page
-title: About
-permalink: /about/
-weight: 2
----
-
-CasjaysDev Security Blog
--- assets/js/fetch-url.js
+++
@@ -1,7 +0,0 @@
-fetch('https://github.com/casjaysdev-sites/static.casjay.pro/raw/main/README.md')
-  .then(function (response) {
-    return response.text()
-  }).then(function (body) {
-    console.log(res)
-    document.body.innerHTML = res
-  })
